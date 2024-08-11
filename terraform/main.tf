d09e18e09699a3be00d65053a5bc045ca759cec5
Kenneth V. Domingo <hello@kvd.studio>
2024-08-11 14:44:15 +0800

    ci: add terraform workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,12 @@
 locals {
-  subdomains_to_expose = toset([
-    "git",
-  ])
+  base_domain = "kvd.studio"
+  subdomains_to_expose = tomap({
+    git = "git.lab.kvd.studio"
+  })
+}
+
+data "cloudflare_zone" "kvd_studio" {
+  name = local.base_domain
 }
 
 resource "cloudflare_tunnel" "lab" {
@@ -29,8 +34,8 @@ resource "cloudflare_tunnel_config" "lab" {
       for_each = local.subdomains_to_expose
 
       content {
-        hostname = "${ingress_rule.value}.lab.kvd.studio"
-        service  = "https://${ingress_rule.value}.lab.kvd.studio"
+        hostname = "${ingress_rule.key}.${local.base_domain}"
+        service  = "https://${ingress_rule.value}"
       }
     }
 
@@ -38,4 +43,14 @@ resource "cloudflare_tunnel_config" "lab" {
       service = "http_status:404"
     }
   }
+}
+
+resource "cloudflare_record" "lab" {
+  for_each = local.subdomains_to_expose
+
+  name    = each.key
+  type    = "CNAME"
+  zone_id = data.cloudflare_zone.kvd_studio.id
+  content = "${cloudflare_tunnel.lab.id}.cfargotunnel.com"
+  proxied = true
 }
